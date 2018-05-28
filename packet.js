13bb9cdaed6dc3e518f0a1f35ce1e37a5b0cd115
RGWantz <ruthanna.wantz17@houghton.edu>
2018-05-28 10:44:15 -0400

    Assignment almost finished

--- packet.js
+++ packet.js
@@ -22,9 +22,33 @@ function fib(n) {
 // Define function: bubbleSort(numArray)
 // Use the bubble sort algorithm to sort the array.
 // Return the sorted array.
+function bubbleSort(numArray) {
+    for(let i = 0; i <numArray.length; i++) {
+        for(let k = 1; k < numArray.length; k++) {
+            if(numArray[k-1] > numArray[k]) {
+                let c = numArray[k-1];
+                numArray[k-1] = numArray[k]; 
+                numArray[k] = c; 
+            }
+        }
+    }
+    return numArray; 
+}
+// let myArray2 = [65, 13, 0, 28, 4];
+// console.log(bubbleSort(myArray2)); 
+
 // 3. Reverse String
 // Define function: reverseStr(someStr)
 // Reverse and return the String. 
+function reverseStr(someStr) {
+    let newStr = ''; 
+    for(let i = (someStr.length-1); i >= 0; i--) {
+        newStr += someStr.charAt(i); 
+    }
+    return newStr; 
+}
+//console.log(reverseStr('Charlie Brown')); 
+
 // 4. Factorial
 // Define function: factorial(someNum)
 // Use recursion to compute and return the factorial of someNum.
@@ -38,28 +62,70 @@ function factorial(someNum) {
     return fac;
 }
 //console.log(factorial(5)); //This does not actually use recursion. I have seen it before... 
+
 // 5. Substring
 // Define function substring(someStr, length, offset)
 // Return the substring contained between offset and (offset + length) inclusively. 
 // If incorrect input is entered, use the alert function and describe why the input was incorrect. 
+function substring(someStr, length, offset) {
+    let newStr = ''; 
+    if(length > offset) {
+        console.log('Parameters out of order. Please try again'); 
+    } else if(offset > (someStr.length - 1) ) {
+        console.log('Parameter is out of bounds. Please try again'); 
+    } else {
+        newStr = someStr.substring(length, offset); 
+        newStr += someStr.charAt(offset); 
+    }
+    return newStr; 
+}
+// console.log(substring('JasperJohannes', 5, 10));
+// console.log(substring('JasperJohannes', 5, 15));
+// console.log(substring('JasperJohannes', 15, 10));
 
 // 6. Even Number
 // Define function: isEven(someNum)
 // Return true if even, false if odd.
 // Do not use % operator.
-function isEven(someNum) { //This would find powers of 2... and doesn't work anyway. 
-    let n = someNum;
-    while(isEven(n)=== false){
-        if(n === 1){
-            return true; 
-        } else return false;
-        n = n/2; 
-    }
-} //can I get (or can I build) a function that checks to see if something is a whole number? Can I round? Can I snip off decimals or non-decimals? 
+function isEven(someNum) { 
+    let n = (someNum / 2);
+    if(n === Math.round(n)){
+        return true;
+        
+    } else return false; 
+}  
+// console.log(isEven(17)); 
+// console.log(isEven(42)); 
+// console.log(isEven(20.15)); 
 
 // 7. Palindrome
 // Define function isPalindrome(someStr)
 // Return true if someStr is a palindrome, otherwise return false
+function isPalindrome(str) {
+    let strLen = str.length; 
+    str = str.toLowerCase();
+    let fIdx = 0, bIdx = (strLen - 1); 
+    let palindrome = true;
+    while (fIdx < bIdx) {
+        let c1 = str.charAt(fIdx);
+        let c2 = str.charAt(bIdx);
+        if (c1 != c2) {
+            palindrome = false; 
+            console.log(`"${str}" is not a palindrome.`);
+            break; 
+        } else {
+            ++fIdx;
+            --bIdx;
+        }
+    }
+    if (palindrome == true){
+        console.log(`"${str}" is a palindrome.`);
+    }
+    return palindrome; 
+} 
+//console.log(isPalindrome('Step On No Pets')); 
+//console.log(isPalindrome('Ma has a Ham')); 
+
 // 8. Shapes
 // Define function: printShape(shape, height, character)
 // shape is a String and is either "Square", "Triangle", "Diamond".
@@ -93,7 +159,10 @@ function printShape(shape, height, character) {
         }
         break; 
     case 'Diamond':
-        //verify that height is odd, with isEven funtion or at least % 2. 
+        if(isEven(height)) {
+            console.log('Height of Diamond must be odd.')
+            break; 
+        }
         size = 1; 
         let amnt = 0; 
         while(size <= height) {
@@ -135,7 +204,8 @@ function printShape(shape, height, character) {
 }
 //printShape('Square', 3, 0); 
 //printShape('Triangle', 7, 0); 
-//printShape('Diamond', 9, '@'); 
+// printShape('Diamond', 9, '@'); 
+// printShape('Diamond', 6, '%'); 
 //printShape('Purple', 4, 'Q'); 
 
 // 9. Object literal
@@ -145,7 +215,7 @@ function traverseObject(someObj) {
     console.log(someObj); 
 }
 
-//traverseObject(new ); 
+traverseObject(new Person('Ruthanna', 23)); 
 
 // 10. Delete Element
 // Define function deleteElement(someArr)
@@ -153,7 +223,7 @@ function traverseObject(someObj) {
 // Delete the third element in the array.
 // Print length
 // The lengths should be the same. 
-// No, they should not! 
+ 
 function deleteElement(someArr) {
     console.log(`The initial length of the array is ${someArr.length}`); 
     delete(someArr[2]);
@@ -179,9 +249,24 @@ function spliceElement(someArr) {
 // 12. Defining an object using a constructor
 // Define a function Person(name, age)
 // The following line should set a Person object to the variable john:
-//     var john = new Person("John", 30);
+function Person(name, age) {
+    this.name = name; 
+    this.age = age; 
+}
+// var john = new Person("John", 30);
+// 
 
 // 13. Defining an object using an object literal
 // Define function getPerson(name, age)
 // The following line should set a Person object to the variable john:
-//     var john = getPerson("John", 30);
+function getPerson(name, inAge) {
+    let personLit = {
+        name: '', 
+        age: 0
+    }
+    personLit.name = name; 
+    personLit.age = inAge; 
+    return personLit; 
+}
+// var john = getPerson("John", 30);
+// console.log(john); 
