dc5482fb8c98973eafe798e4323795ba019172cc
Fernando Hudson <fernando_hudson@redlands.edu>
2018-05-28 00:13:17 -0400

    Updated homework 5/28 -FH

--- packet.js
+++ packet.js
@@ -6,68 +6,71 @@
 // 1. Fibonacci
 // Define function: fib(n) 
 // Return the nth number in the fibonacci sequence.
-// function fib(n) {
-//     if (n < 2) {
-//       return 1;
-//     } else {
-//       return fib(n - 2) + fib(n - 1);
-//     }
-//   }
-//   console.log(fib(0))
-//   console.log(fib(1))
+
+function fib(n) {
+    if (n < 2) {
+      return 1;
+    } else {
+      return fib(n - 2) + fib(n - 1);
+    }
+  }
+  console.log(fib(0))
+  console.log(fib(1))
 
 // 2. Bubble Sort
 // Define function: bubbleSort(numArray)
 // Use the bubble sort algorithm to sort the array.
 // Return the sorted array.
 
-// let a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
+let a = [33, 103, 3, 726, 200, 984, 198, 764, 9];
 
-// function bubbleSort(numArray)
-// {
-//     let swapped;
-//     do {
-//         swapped = false;
-//         for (let i=0; i < numArray.length-1; i++) {
-//             if (numArray[i] > numArray[i+1]) {
-//                 let temp = numArray[i];
-//                 numArray[i] = numArray[i+1];
-//                 numArray[i+1] = temp;
-//                 swapped = true;
-//             }
-//         }
-//     } while (swapped);
-// }
+function bubbleSort(numArray)
+{
+    let swapped;
+    do {
+        swapped = false;
+        for (let i=0; i < numArray.length-1; i++) {
+            if (numArray[i] > numArray[i+1]) {
+                let temp = numArray[i];
+                numArray[i] = numArray[i+1];
+                numArray[i+1] = temp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+}
 
-// bubbleSort(a);
-// console.log(a);   
+bubbleSort(a);
+console.log(a);   
 
 // 3. Reverse String
 // Define function: reverseStr(someStr)
 // Reverse and return the String.
-// function reverseStr(someStr) {
 
-//     let splitString = someStr.split("");
-//     let reverseArray = splitString.reverse(); 
-//     let joinArray = reverseArray.join(""); 
-//     console.log(joinArray); 
-// }
+function reverseStr(someStr) {
+
+    let splitString = someStr.split("");
+    let reverseArray = splitString.reverse(); 
+    let joinArray = reverseArray.join(""); 
+    console.log(joinArray); 
+}
  
-// reverseStr("hello");
+reverseStr("hello");
 
 // 4. Factorial
 //Define function: factorial(someNum)
 // Use recursion to compute and return the factorial of someNum.
-// function factorial(someNum){
-//     if (someNum === 0) {
-//         return 1;
-//     } else {
-//         return someNum * factorial(someNum-1);
-//     }
-// }
-// console.log(factorial(0));
-// console.log(factorial(1));
-// console.log(factorial(2));
+
+function factorial(someNum){
+    if (someNum === 0) {
+        return 1;
+    } else {
+        return someNum * factorial(someNum-1);
+    }
+}
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
 
 
 // 5. Substring
@@ -75,61 +78,64 @@
 //Define function substring(someStr, length, offset)
 // Return the substring contained between offset and (offset + length) inclusively.
 // If incorrect input is entered, use the alert function and describe why the input was incorrect.
-// let name = 'cruelworld';
 
-// function subString(someStr, length, offset){
-//     if (isNaN(someStr)){
-//         let ss= '';
-//     for (let  i = offset; i < offset+length; i++){
-//         //console.log(someStr.charAt(i));
-//         ss += someStr.charAt(i)
-//         }
-//         console.log(ss);
+let name = 'cruelworld';
 
-//     }
-//     else{ 
-//         console.log('String can not be a number!')
-//     }
-// }
+function subString(someStr, length, offset){
+    if (isNaN(someStr)){
+        let ss= '';
+    for (let  i = offset; i < offset+length; i++){
+        //console.log(someStr.charAt(i));
+        ss += someStr.charAt(i)
+        }
+        console.log(ss);
 
-// subString(name, 4, 1);
+    }
+    else{ 
+        console.log('String can not be a number!')
+    }
+}
 
-///////// 6. Even Number//////////////////////////////////////////////////////////////////
+subString(name, 4, 1);
+
+//6. Even Number
 // Define function: isEven(someNum)
 // Return true if even, false if odd.
 // Do not use % operator.
-//  function isEven(someNum){
-//      let iNum = someNum;
-//     for (let i=0; i < (someNum/2); i++){
-//         iNum = iNum - 2;
-//     }
-//     if (iNum === 0){
-//         return true;
-//         //console.log(someNum + ' is even.')
-//     } else {
-//         return false;
-//         //console.log(someNum + ' is odd.')
-//     }
-// }
-// console.log(isEven(3));
+
+function isEven(someNum){
+     let iNum = someNum;
+    for (let i=0; i < (someNum/2); i++){
+        iNum = iNum - 2;
+    }
+    if (iNum === 0){
+        return true;
+        //console.log(someNum + ' is even.')
+    } else {
+        return false;
+        //console.log(someNum + ' is odd.')
+    }
+}
+console.log(isEven(3));
 
 // 7. Palindrome
 // Define function isPalindrome(someStr)
 // Return true if someStr is a palindrome, otherwise return false
-// let string = 'car'
-// function isPalindrome(someStr){
 
-//     let splitString = someStr.split("");
-//     let reverseArray = splitString.reverse(); 
-//     let joinArray = reverseArray.join(""); 
-//     if (someStr === joinArray){
-//         console.log(someStr + ' is a palindrome.')
-//     }else{
-//         console.log(someStr + ' is not a palindrome.')
-//     }
-// }
+let string = 'car'
+function isPalindrome(someStr){
 
-// isPalindrome(string)
+    let splitString = someStr.split("");
+    let reverseArray = splitString.reverse(); 
+    let joinArray = reverseArray.join(""); 
+    if (someStr === joinArray){
+        console.log(someStr + ' is a palindrome.')
+    }else{
+        console.log(someStr + ' is not a palindrome.')
+    }
+}
+
+isPalindrome(string)
 
 // 8. Shapes
 // Define function: printShape(shape, height, character)
@@ -152,55 +158,69 @@
 // *****
 //  ***
 //   *
-// function printShape(shape, height, character){
-//     switch(shape){
-//         case 'square':
-//         for (let i = 1; i <= height; i++) {
-//             for (let j = height; j <= height; j++){
-//                 console.log(character.repeat(j))
-//             }
-//         }
-//         break;
 
-//         case 'triangle':
-//         for (let i = 1; i <= height; i++) {
-//             console.log(character.repeat(i))
-//         }
-//         break;
-//         case 'diamond':
-//             let arr = Array();
-//             let space = height -2;
+function printShape(shape, height, character){
+    switch(shape){
+        case 'square':
+        for (let i = 1; i <= height; i++) {
+            for (let j = height; j <= height; j++){
+                console.log(character.repeat(j))
+            }
+        }
+        break;
+
+        case 'triangle':
+        for (let i = 1; i <= height; i++) {
+            console.log(character.repeat(i))
+        }
+        break;
+        case 'diamond':
+            let arr = Array();
+            let space = height -2;
         
-//             for (let i = 1; i <= height; i++) {
-//                 arr += ' '.repeat(space)
-//                 arr+= character.repeat(i++)
-//                 console.log(arr);
-//                 arr = new Array();
-//                 space--;
-//             }
-//             space +=2;
-//             for (let i = height-2; i >= 1; i--) {
-//                 arr += ' '.repeat(space)
-//                 arr+= character.repeat(i--)
-//                 console.log(arr);
-//                 arr = new Array();
-//                 space++;
-//             }
-//             break;
-//             default:            
-//             console.log('Invalid shape!')
-//     }
-// }
-// printShape('square', 9, "$");
+            for (let i = 1; i <= height; i++) {
+                arr += ' '.repeat(space)
+                arr+= character.repeat(i++)
+                console.log(arr);
+                arr = new Array();
+                space--;
+            }
+            space +=2;
+            for (let i = height-2; i >= 1; i--) {
+                arr += ' '.repeat(space)
+                arr+= character.repeat(i--)
+                console.log(arr);
+                arr = new Array();
+                space++;
+            }
+            break;
+            default:            
+            console.log('Invalid shape!')
+    }
+}
+printShape('square', 9, "$");
 
 
 // 9. Object literal
 // Define function traverseObject(someObj)
 // Print every property and it's value.
-// function traverseObject(someObj){
 
-// }
-// traverseObject(someObj);
+function Dog(name, color, size){
+    this.name = name;
+    this.color = color;
+    this.size = size;
+}
+
+let dog = new Dog('Doug', 'Brown', 'Small');
+
+ function traverseObject(someObj){
+
+    for (let property in someObj){
+        console.log(`${property} = ${someObj[property]}`)
+    }
+ }
+
+ traverseObject(dog);
 
 // 10. Delete Element
 // Define function deleteElement(someArr)
@@ -209,6 +229,17 @@
 // Print length
 // The lengths should be the same.
 
+let cars = new Array('car1', 'car2', 'car3', 'car4', 'car5');
+
+function deleteElement(someArr){
+console.log(`Length of the array is ${cars.length}`);
+console.log(`The array holds ${cars}`);
+delete cars[2];
+console.log(`The array holds after deleting third element ${cars}`);
+console.log(`Length of the array is ${cars.length}`);
+}
+deleteElement(cars);
+
 // 11. Splice Element
 // Define function spliceElement(someArr)
 // Print length
@@ -216,12 +247,43 @@
 // Print length
 // The lengths should be one less than the original length.
 
+ let cars = new Array('car1', 'car2', 'car3', 'car4', 'car5');
+
+function spliceElement(someArr){
+    console.log(`Length of the array is ${cars.length}`);
+    console.log(`The array holds ${cars}`);
+    casrs.splice(2,1);
+    console.log(`The array holds after deleting third element ${cars}`);
+    console.log(`Length of the array is ${cars.length}`);
+}
+
+spliceElement(cars);
+
 // 12. Defining an object using a constructor
 // Define a function Person(name, age)
 // The following line should set a Person object to the variable john:
-//     var john = new Person("John", 30);
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+    let john = new Person("John", 30);
+    console.log(john);
+
 
 // 13. Defining an object using an object literal
 // Define function getPerson(name, age)
 // The following line should set a Person object to the variable john:
-//     var john = getPerson("John", 30);
+
+function getPerson(name, age){
+    let person1 = {
+        name: name,
+        age: age
+    }
+    return person1;
+}
+
+ let john = getPerson("John", 30);
+
+console.log(john);
