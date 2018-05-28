1e8a6dfb8d2882dbe8d192e543fdc513dc9251da
Bino Philip <binophilip123@gmail.com>
2018-05-28 19:05:42 -0400

    JS Assignment Update

--- js-assignment.js
+++ js-assignment.js
@@ -13,20 +13,60 @@ function fib(n){
 // Define function: bubbleSort(numArray)
 // Use the bubble sort algorithm to sort the array.
 // Return the sorted array.
-
+function bubbleSort(numArray) {
+    
+    for (let i = numArray.length -1; i >= 0; i--){
+        for (let j = 0; j <= i; j++) {
+            if (numArray[j + 1] < numArray[j]) {
+                let temp = numArray[j];
+                numArray[j] = numArray[j + 1];
+                numArray[j + 1] = temp;
+            }
+        }
+    }
+    return numArray;
+    }
+    
+    var array = [4,1,3,0,2];
+    console.log(bubbleSort(array));
 // 3. Reverse String
 // Define function: reverseStr(someStr)
 // Reverse and return the String.
+function reverseStr(someStr) {
+    let reversedString = someStr.split('').reverse().join('');
+    return reversedString;
+}
 
+console.log(reverseStr('ab c d'));
 // 4. Factorial
 // Define function: factorial(someNum)
 // Use recursion to compute and return the factorial of someNum.
-
+function factorial(someNum){
+    if (someNum === 0){
+        return 1;
+    }else{
+        return someNum * factorial(someNum - 1);
+    }
+}
+console.log(factorial(4));
 // 5. Substring
 // Define function substring(someStr, length, offset)
 // Return the substring contained between offset and (offset + length) inclusively.
 // If incorrect input is entered, use the alert function and describe why the input was incorrect.
+function substring(someStr, length, offset){
+    if(length>someStr.length){
+        console.log('Input length is higher than the actual string length.');
+        return null;
+    }else if(offset>someStr.length || offset<0){
+        console.log('The inputted offset does not exist within the corresponding string.');
+        return null;
+    }else{
+        let end = (length+offset);
+        return b = someStr.substring(offset-1,end);
+    }
+}
 
+console.log(substring('bino philip',3,4));
 // 6. Even Number
 // Define function: isEven(someNum)
 // Return true if even, false if odd.
@@ -47,7 +87,19 @@ console.log(isEven(9));
 // 7. Palindrome
 // Define function isPalindrome(someStr)
 // Return true if someStr is a palindrome, otherwise return false
+function isPalindrome(someStr) {
+    let tempString = someStr.toLowerCase().replace(/[^a-zA-Z/s]/g,'');
+    console.log(tempString);
+    let reversedString = tempString.split('').reverse().join('');
 
+    if (reversedString == tempString){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(isPalindrome('Was, It A Rat I Saw?'));
 // 8. Shapes
 // Define function: printShape(shape, height, character)
 // shape is a String and is either "Square", "Triangle", "Diamond".
@@ -69,7 +121,34 @@ console.log(isEven(9));
 // *****
 //  ***
 //   *
+function printShape(shape, height, character){
+    switch(shape){
+    case 'Square':
+       for(let i=0;i<height;i++){
+            let square = ""; 
+            for (let j = 0; j < height; j++) {
+                square += character;
+            }
+            console.log(square);
+       }
+       break;
+    case 'Triangle':
+        for (let i = height; i >= 1; i--) {
+            let triangle = "";
+            for (let j = i; j <= height; j++) {
+                triangle += character;
+           }
+            console.log(triangle);
+        }
+        break;
+    case 'Diamond':
 
+        break;
+    }
+}
+
+printShape('Square',3,'$');
+printShape('Triangle',3,'$');
 // 9. Object literal
 // Define function traverseObject(someObj)
 // Print every property and it's value.
@@ -78,7 +157,6 @@ function traverseObject(someObj){
     for (property in someObj) {
         console.log(`${property}: ${someObj[property]}`);
     }
-
 }
 
 let person = {firstName:"Bino", lastName:"Philip", age:25}; 
